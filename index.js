52d15ba1498a9bdb3a0dce66d2086835ee8d2586
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-04-16 19:11:41 +0300

    fetch commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+const body = document.querySelector("body");
+
+function submitData(userName, userEmail) {
+  const data = {
+    name: userName,
+    email: userEmail,
+  };
+
+  const object = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  return fetch("http://localhost:3000/users", object)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const div = document.createElement("div");
+      div.innerHTML = "ID: " + object.id;
+      body.append(div);
+    })
+    .catch(function (error) {
+      const divError = document.createElement("div");
+      divError.innerHTML = "Unauthorized Access";
+      body.append(divError);
+    });
+}
+
+submitData(hel, "eg");
